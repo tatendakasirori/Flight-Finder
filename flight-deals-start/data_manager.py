be6dcb816a961fbf61d11d0c4b8decc16f6daf3e
Tatenda Kasirori <tatendakasirori1000@gmail.com>
2025-09-27 17:52:15 -0500

    initial commit

---
+++ flight-deals-start/data_manager.py
@@ -0,0 +1,7 @@
+class DataManager:
+    #This class is responsible for talking to the Google Sheet.
+    pass
+
+# 1. define functions to talk to google sheet, eg retreving current information about 
+# flight data and the like
+#2. 
