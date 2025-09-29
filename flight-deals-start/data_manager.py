fbf335dda88e7194d56af8a271276363e0f682ac
Tatenda Kasirori <tatendakasirori1000@gmail.com>
2025-09-29 07:03:02 -0500

    data_manager complete,flight search partialy

--- flight-deals-start/data_manager.py
+++ flight-deals-start/data_manager.py
@@ -1,7 +1,23 @@
+import requests
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self,url,auth):
+        self.url = url
+        self.auth = auth
+        self.headers = sh_headers={
+        'Authorization': auth,
+        "Content-Type": "application/json"
+        }
 
-# 1. define functions to talk to google sheet, eg retreving current information about 
-# flight data and the like
-#2. 
+    # gets a list of all cities in the sheet
+    def get_cities(self):
+        response = requests.get(url=self.url,headers=self.headers)
+        prices = response.json()['prices']
+        city_list = [i['city'] for i in prices]
+        return city_list
+
+
+
+
+
--- flight-deals-start/main.py
+++ flight-deals-start/main.py
@@ -1,1 +1,13 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+from dotenv import load_dotenv
+import os
+from data_manager import DataManager
+
+load_dotenv()
+
+sheety_data = DataManager(url=os.getenv("SH_URL"),auth=os.getenv("SH_AUTH"))
+
+list = sheety_data.get_cities()
+
+print(list)
