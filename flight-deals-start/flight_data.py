27cf9dc248f3e5023ef51d9b8189d35afda31d7e
Tatenda Kasirori <tatendakasirori1000@gmail.com>
2025-10-15 23:42:04 -0500

    defining flights_summary function

--- flight-deals-start/flight_data.py
+++ flight-deals-start/flight_data.py
@@ -13,26 +13,27 @@ class FlightData:
         arrival:___
         }
         ] for notification_manager'''
-        filtered_json_list = self.filter_by_price(iataCode_flights)# filter first
+        filtered_flights_list = self.filter_by_price(iataCode_flights)# filter first
         print("Summarizing flights")
         flight_summary = []
-        for json_file in filtered_json_list:
-            flight_discription = {}
-            flight_discription['source'] = json_file.get('data')[0]
+        for flight_dict in filtered_flights_list:
+            flight_description = {}
+            flight_description['source'] = flight_dict['itineraries'][0]['segments'][0]['departure']
 
     def filter_by_price(self,iataCode_flights:dict):
         print('filtering by price')
-        filtered_json_list = [] # collect filtered iataCode_flights 
-        for city,iata_price in self.citydata_dict.items():
+        filtered_flights = [] # collect filtered iataCode_flights 
+        for iata_price in self.citydata_dict.values():
             for json_file in iataCode_flights.get(iata_price[0],{}):
                 for flight in json_file['data']: #loop over flights in data list
-                    print('price is : ' + float(flight.get('price',{}).get('base',float('inf'))))
-                    if float(flight.get('price',{}).get('base',float('inf'))) <= iata_price[1]:
-                        filtered_json_list.append(flight) #adding each cheap flight 
+                    print('price is : ' + str(float(flight.get('price',{}).get('base',float('inf')))))
+                    if float(flight.get('price',{}).get('total',float('inf'))) <= iata_price[1]:
+                        filtered_flights.append(flight) #adding each cheap flight
+                        print('Cheap flight!')
                     else:
                         print('Too expensive')
         print('Flight data filtered!!!')
-        return filtered_json_list
+        return filtered_flights
 
 
 
